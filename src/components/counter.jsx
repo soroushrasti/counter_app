d5388d88709f1f678bdb1041d4168051e85bb89b
Soroush Rasti <rasti.soroush@gmail.com>
2020-10-30 18:08:02 +0330

    add handleincrement

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
-class counter extends Component {
+class Counter extends Component {
     formatCount(){
         const {count}=this.state;
         return count ===0 ? 'Zero' : count;
     }
     styles={size:10, fontWeigth: 'bold'};
-
-    state = { count:0}
+    state = { 
+        count:0,
+        tags:['tag1','tag2','tag3']
+    }
+    renderTags(){
+        return <ul>
+            {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+        </ul>
+    }
+    handleIncrement=() =>{
+        this.setState{
+            count: this.state.count +1;
+        }
+    }
+ 
     render() { 
-        let classes = this.getBadgeClasses();
         return (  <div>
-            <span style={this.style} className={classes}>
+            <span style={this.style} className={this.getBadgeClasses()}>
              {this.formatCount()}
             </span>
-            <button className='btn'>Increment</button>
+            <button onClick={()=> this.handleIncrement(product) } className='btn btn-secondary btn-sm'>Increment</button>
+            {this.state.tags.length===0 && "Please entera tag"}
+            {this.renderTags()}
         </div>);
     }
 
@@ -24,4 +38,4 @@ class counter extends Component {
     }
 }
  
-export default counter;
+export default Counter;
