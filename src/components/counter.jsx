372b5bc8e7a75337fe458f60d4744cb59651a1a1
Soroush Rasti <rasti.soroush@gmail.com>
2020-11-01 18:02:34 +0330

    adding counters

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -14,11 +14,11 @@ class Counter extends Component {
             {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
         </ul>
     }
-    handleIncrement=() =>{
-        this.setState{
-            count: this.state.count +1;
-        }
-    }
+    handleIncrement= (product) =>{
+        this.setState({
+            count: this.state.count +1
+        });
+    };
  
     render() { 
         return (  <div>
@@ -26,7 +26,7 @@ class Counter extends Component {
              {this.formatCount()}
             </span>
             <button onClick={()=> this.handleIncrement(product) } className='btn btn-secondary btn-sm'>Increment</button>
-            {this.state.tags.length===0 && "Please entera tag"}
+            {this.state.tags.length===0 && "Please enter a tag"}
             {this.renderTags()}
         </div>);
     }
